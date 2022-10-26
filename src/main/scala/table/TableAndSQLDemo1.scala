c3550e5e2324fea35658d08d0e958bd356bf5503
Sunda-Yy <1491895830@qq.com>
2022-10-26 17:55:10 +0800

    Update TableAndSQLDemo1.scala

--- src/main/scala/table/TableAndSQLDemo1.scala
+++ src/main/scala/table/TableAndSQLDemo1.scala
@@ -1,11 +1,11 @@
 package table
 
-//import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.table.api.Expressions.$
 import org.apache.flink.table.api._
 
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.table.api.bridge.java.StreamTableEnvironment
-import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
+import org.apache.flink.api.scala._
+import org.apache.flink.table.api._
+//import org.apache.flink.table.api.bridge.scala._
 
 object TableAndSQLDemo1 {
   def main(args: Array[String]): Unit = {
@@ -17,6 +17,41 @@ object TableAndSQLDemo1 {
       .build()
 
     val tEnv = TableEnvironment.create(settings)
+
+    tEnv.executeSql(
+      """
+        |create table t_name_from(
+        | field1 type,
+        | field2 type,
+        | field3 type,
+        | field4 type
+        | )with(
+        |   'connector' = 'filesystem'
+        |   'path' = 'file:///文件来源路径（记得路径是/）'
+        |   'format' = 'csv'
+        | )
+        |""".stripMargin)
+    //
+
+    //创建一张输出表关联外部组件（输出到什么文件）
+    tEnv.executeSql(
+      """
+        |create table t_name_to(
+        | field1 type,
+        | field2 type,
+        | field3 type,
+        | field4 type
+        | )with(
+        |   'connector' = 'filesystem'
+        |   'path' = 'file:///文件来源路径（记得路径是/）'
+        |   'format' = 'csv'
+        | )
+        |""".stripMargin)
+
+    tEnv.from("t_name_from").select($("field1"), $("field2"),$("field3")).where("field1 > 1000")
+
+//    tab.filter($"name" === "Fred")
+//    tab.where("name = 'Fred'")
 
 
   }
