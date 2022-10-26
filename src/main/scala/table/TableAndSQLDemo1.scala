10cdcdc9c04512679303ce76ae5bbab68e8c016a
Sunda-Yy <1491895830@qq.com>
2022-10-27 00:06:04 +0800

    sql
    kafkasql

--- src/main/scala/table/TableAndSQLDemo1.scala
+++ src/main/scala/table/TableAndSQLDemo1.scala
@@ -1,11 +1,9 @@
 package table
 
 import org.apache.flink.table.api.Expressions.$
+import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
 import org.apache.flink.table.api._
 
-import org.apache.flink.api.scala._
-import org.apache.flink.table.api._
-//import org.apache.flink.table.api.bridge.scala._
 
 object TableAndSQLDemo1 {
   def main(args: Array[String]): Unit = {
@@ -31,7 +29,7 @@ object TableAndSQLDemo1 {
         |   'format' = 'csv'
         | )
         |""".stripMargin)
-    //
+
 
     //创建一张输出表关联外部组件（输出到什么文件）
     tEnv.executeSql(
@@ -48,11 +46,18 @@ object TableAndSQLDemo1 {
         | )
         |""".stripMargin)
 
-    tEnv.from("t_name_from").select($("field1"), $("field2"),$("field3")).where("field1 > 1000")
+    //tableAPI查询
+    val table1 = tEnv.from("t_name_from").select($("field1"), $("field2"), $("field3")).where("field1 > 1000")
 
-//    tab.filter($"name" === "Fred")
-//    tab.where("name = 'Fred'")
+    //
+    val table2 = tEnv.sqlQuery(
+      """
+        |select field1,field1,field1
+        |from t_name_from
+        |where filed1 ....
+        |""".stripMargin)
 
+    table2.executeInsert("t_name_to")
 
   }
 }
---
+++ src/main/scala/table/TableAndSQLDemo2.scala
@@ -0,0 +1,58 @@
+package table
+
+import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
+
+object TableAndSQLDemo2 {
+  def main(args: Array[String]): Unit = {
+    val settings = EnvironmentSettings
+      .newInstance()
+      .inStreamingMode()
+      //.inBatchMode()
+      .build()
+
+    val tEnv = TableEnvironment.create(settings)
+
+    //输入数据对接kafka
+    tEnv.executeSql(
+      """
+        |create table t_name_from(
+        | field1 type,
+        | field2 type,
+        | field3 type,
+        | field4 type
+        | )with(
+        |  'connector' = 'kafka',
+        |  'topic' = 'user_behavior',
+        |  'properties.bootstrap.servers' = 'localhost:9092',
+        |  'properties.group.id' = 'testGroup',
+        |  'scan.startup.mode' = 'earliest-offset',
+        |  'format' = 'csv'
+        | )
+        |""".stripMargin)
+
+
+    //输出数据对接kafka（）要有主键，避免插入重复？
+    tEnv.executeSql(
+      """
+        |create table t_name_to(
+        | field1 type,
+        | field2 type,
+        | field3 type,
+        | field4 type
+        | PRIMARY KEY(filed1) NOT ENFORCED
+        | )with(
+        |  'connector' = 'kafka',
+        |  'topic' = 'user_behavior',
+        |  'properties.bootstrap.servers' = 'localhost:9092',
+        |  'key.format' = 'json'
+        |  'value.format' = 'json'
+        | )
+        |""".stripMargin)
+    //        |  'properties.group.id' = 'testGroup',
+    //        |  'scan.startup.mode' = 'earliest-offset',
+    //        |  'format' = 'csv'
+
+
+
+  }
+}
